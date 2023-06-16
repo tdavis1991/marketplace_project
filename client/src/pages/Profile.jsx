b9addc94f2e7e794f3edf8aec49d4cf69d006db0
tevin davis <aod.td1105@gmail.com>
2023-06-16 06:39:46 -0400

    working on item details and profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Item from './ItemForm';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 const Profile = () => {
@@ -8,17 +9,18 @@ const Profile = () => {
   console.log('PROFILE', user.user.inventory);
 
   return (
-    <div className='flex justify-between w-full h-screen mt-10'>
-    <div className='flex flex-col'>
+    <div className='flex w-full h-screen gap-3 mt-10'>
+    <div className='flex flex-1 flex-col'>
       <img src={user.user.avatar} alt='avatar' className='w-96 h-96' />
       <h2 className='font-bold'>Contact</h2>
       <h3 className='text-xl'><span className='font-bold'>Name:</span> {user.user.name}</h3>
       <h3 className='text-xl'><span className='font-bold'>Email:</span> {user.user.email}</h3>
       <h1>Inventory</h1>
     </div>
-    <Link to='/create'>
+    {/* <Link to='/create'>
       <button className='text-2xl font-bold bg-quaternary px-5 py-2 rounded-xl'>+ Add Item</button>
-    </Link>
+    </Link> */}
+    <Item />
     </div>
   )
 }
